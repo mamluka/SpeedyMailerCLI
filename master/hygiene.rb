5fc3994d29c0c9f62356985aa1c0900fe84e3611
davidmz <david.mazvovsky@gmail.com>
2013-09-25 10:35:06 -0700

    update clean

--- master/hygiene.rb
+++ master/hygiene.rb
@@ -4,7 +4,7 @@ require 'thor'
 require_relative '../core/jobs'
 
 class Hygiene < Thor
-  desc 'test_clean email', 'Test cleaning of one email'
+  desc 'test_clean email droneId', 'Test cleaning of one email'
 
   def test_clean(recipient,drone_id)
     Sidekiq::Client.push('queue' => drone_id, 'class' => VerityRecipient, 'args' => [recipient])
